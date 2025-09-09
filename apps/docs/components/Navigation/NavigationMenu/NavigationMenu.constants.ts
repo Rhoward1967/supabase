06ed5fda73c6c8ce15409f254835f43d95ec86d6
Filipe Cabaço <filipecabaco@gmail.com>
2025-09-09 21:08:00 +0100

    realtime: improve getting started guide

--- apps/docs/components/Navigation/NavigationMenu/NavigationMenu.constants.ts
+++ apps/docs/components/Navigation/NavigationMenu/NavigationMenu.constants.ts
@@ -1658,8 +1658,8 @@ export const realtime: NavMenuConstant = {
       url: '/guides/realtime',
     },
     {
-      name: 'Concepts',
-      url: '/guides/realtime/concepts',
+      name: 'Getting Started',
+      url: '/guides/realtime/getting_started',
     },
     {
       name: 'Usage',
